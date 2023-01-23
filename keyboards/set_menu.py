73d0322a998a540321c88d115d386ae8d15c6474
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2023-01-23 23:26:27 +0300

    fix

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -8,6 +8,7 @@ async def set_main_menu(dp: Dispatcher):
         BotCommand(command="/help", description="Помощь"),
         BotCommand(command="/calc", description="Калькулятор"),
         BotCommand(command="/dict", description="Словарь"),
+        BotCommand(command="/chatgpd", description="Словарь"),
         BotCommand(command="/rock_paper_scissors", description="Камень, ножницы, бумага"),
         BotCommand(command="/food", description="Заказать блюда"),
         BotCommand(command="/cancel", description="Отменить текущее действие")
