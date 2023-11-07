f659f61dad4a630a296b6ada2aecc0a9d442c4cf
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2023-11-07 19:47:32 +0300

    webapp added

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,9 +1,18 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.filters import Command
 
 from lexicon.lexicon_ru import LEXICON_RU
 
 router = Router()
+
+
+@router.message(Command(commands='lost_points_calc'))
+async def lost_points(message: Message):
+    bt = InlineKeyboardButton(text=LEXICON_RU['open'],
+                              web_app=WebAppInfo(url="https://islamtambiev.github.io/Lost-Points-Calculator/"))
+    kb = InlineKeyboardMarkup(inline_keyboard=[[bt]])
+    await message.answer(text=LEXICON_RU['lost_points_calc'], reply_markup=kb)
 
 
 # Хэндлер для текстовых сообщений, которые не попали в другие хэндлеры
